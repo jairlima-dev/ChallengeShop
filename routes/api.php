2010b05c407aa4993fc12237d406bf5e116d500c
Jair Lima <jairlimamede@hotmail.com>
2021-09-09 18:10:19 -0300

    nova rota api pra imagens dos produtos

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProductImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use App\Http\Controllers\Api\ProductController;
 */
 
 Route::apiResources(['products' => ProductController::class]);
+Route::apiResources(['product_images' => ProductImageController::class]);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
